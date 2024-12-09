2ae2af1bdf70b4edf3fd01994e2865152d499a1c
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2024-12-09 17:50:02 +0100

    Add blood bag farm prisoner interaction option

---
+++ Source/MoreInjuries/MoreInjuries/Patches/Patch_Alert_AwaitingMedicalOperation_AwaitingMedicalOperation.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using MoreInjuries.KnownDefs;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace MoreInjuries.Patches;
+
+[HarmonyPatch(typeof(Alert_AwaitingMedicalOperation), "AwaitingMedicalOperation", MethodType.Getter)]
+public static class Patch_Alert_AwaitingMedicalOperation_AwaitingMedicalOperation
+{
+    internal static void Postfix(ref List<Pawn> __result)
+    {
+        for (int i = __result.Count - 1; i >= 0; i--)
+        {
+            Pawn pawn = __result[i];
+            if (pawn.IsPrisonerOfColony 
+                && pawn.health.surgeryBills.Count == 1 
+                && pawn.health.surgeryBills[0].recipe == KnownRecipeDefOf.ExtractWholeBloodBag 
+                && pawn.guest.IsInteractionEnabled(KnownPrisonerInteractionModeDefOf.BloodBagFarm))
+            {
+                __result.RemoveAt(i);
+            }
+        }
+    }
+}
---
+++ Source/MoreInjuries/MoreInjuries/Patches/Patch_ITab_Pawn_Visitor_NonExclusiveInteractionToggled.cs
@@ -0,0 +1,55 @@
+﻿using HarmonyLib;
+using MoreInjuries.HealthConditions.HeavyBleeding.Transfusions;
+using MoreInjuries.KnownDefs;
+using RimWorld;
+using Verse;
+
+namespace MoreInjuries.Patches;
+
+[HarmonyPatch(typeof(ITab_Pawn_Visitor), nameof(ITab_Pawn_Visitor.NonExclusiveInteractionToggled))]
+public static class Patch_ITab_Pawn_Visitor_NonExclusiveInteractionToggled
+{
+    internal static void Postfix(PrisonerInteractionModeDef mode, bool enabled)
+    {
+        Logger.LogDebug($"ITab_Pawn_Visitor.NonExclusiveInteractionToggled: {mode.defName} {enabled}");
+
+        if (Find.Selector.SingleSelectedThing is not Pawn { Spawned: true } pawn)
+        {
+            return;
+        }
+        if (enabled && (mode == KnownPrisonerInteractionModeDefOf.BloodBagFarm || mode == PrisonerInteractionModeDefOf.HemogenFarm))
+        {
+            (PrisonerInteractionModeDef otherMode, RecipeDef otherRecipe) = mode == KnownPrisonerInteractionModeDefOf.BloodBagFarm
+                ? (PrisonerInteractionModeDefOf.HemogenFarm, RecipeDefOf.ExtractHemogenPack)
+                : (KnownPrisonerInteractionModeDefOf.BloodBagFarm, KnownRecipeDefOf.ExtractWholeBloodBag);
+
+            if (pawn.guest.IsInteractionEnabled(otherMode))
+            {
+                Messages.Message($"{mode.label} and {otherMode.label} are mutually exclusive.", pawn, MessageTypeDefOf.RejectInput);
+                pawn.guest.ToggleNonExclusiveInteraction(otherMode, false);
+                pawn.BillStack?.Bills?.RemoveAll(b => b.recipe == otherRecipe);
+            }
+        }
+        if (mode != KnownPrisonerInteractionModeDefOf.BloodBagFarm)
+        {
+            return;
+        }
+        Bill? bill = pawn.BillStack?.Bills?.Find(b => b.recipe == KnownRecipeDefOf.ExtractWholeBloodBag);
+        if (enabled)
+        {
+            if (bill is not null || !Recipe_ExtractBloodBag.CanSafelyBeQueued(pawn))
+            {
+                return;
+            }
+            HealthCardUtility.CreateSurgeryBill(pawn, KnownRecipeDefOf.ExtractWholeBloodBag, part: null!);
+        }
+        else
+        {
+            if (bill is null)
+            {
+                return;
+            }
+            pawn.BillStack!.Bills.Remove(bill);
+        }
+    }
+}
