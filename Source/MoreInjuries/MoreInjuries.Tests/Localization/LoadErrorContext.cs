a25a2abe8b8e5fecef19c2a4a054c824da5242af
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2025-01-12 16:11:09 +0100

    Validate language file completeness through unit test project

---
+++ Source/MoreInjuries/MoreInjuries.Tests/Localization/LoadErrorContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+namespace MoreInjuries.Tests.Localization;
+
+public class LoadErrorContext
+{
+    public List<string> Errors { get; } = [];
+
+    public StringBuilder Builder { get; } = new();
+}
