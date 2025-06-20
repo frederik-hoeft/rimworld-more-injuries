d534d6e95075eeff03035b2a5b8620691b5ce65c
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2025-06-20 20:44:25 +0200

    add TODOs

--- Source/MoreInjuries/MoreInjuries/HealthConditions/Secondary/HediffComp_CausedBy.cs
+++ Source/MoreInjuries/MoreInjuries/HealthConditions/Secondary/HediffComp_CausedBy.cs
@@ -3,6 +3,10 @@ using Verse;
 
 namespace MoreInjuries.HealthConditions.Secondary;
 
+// TODO: support multiple causes (e.g., when multiple hypoxia hediffs are compressed into one hediff)
+// in this case it should show "Caused by: <cause1>, <cause2>, ..." in the tooltip
+// as soon as there are multiple causes, stage labels should be stripped. E.g., no "Caused by: <cause1> (Stage 1), <cause1> (Stage 2)"
+// TODO: also, maybe distinguish between different types of hypoxia?
 public class HediffComp_CausedBy : HediffComp
 {
     private string? _causedBy;
