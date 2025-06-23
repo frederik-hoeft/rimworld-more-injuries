92852a96153987be7f9eccfda6acca0a1436bf46
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2025-06-23 21:09:51 +0200

    simplify outcome doer

--- Source/MoreInjuries/MoreInjuries/HealthConditions/MechaniteTherapy/HediffComp_MechaniteTherapy_OutcomeDoer.cs
+++ Source/MoreInjuries/MoreInjuries/HealthConditions/MechaniteTherapy/HediffComp_MechaniteTherapy_OutcomeDoer.cs
@@ -1,19 +1,13 @@
-﻿using MoreInjuries.Extensions;
-using Verse;
+﻿using Verse;
 
 namespace MoreInjuries.HealthConditions.MechaniteTherapy;
 
-public abstract class HediffComp_MechaniteTherapy_OutcomeDoer : IExposable, ILoadReferenceable
+public abstract class HediffComp_MechaniteTherapy_OutcomeDoer : IExposable
 {
-    private string? _loadID;
-
     public abstract void DoOutcome(HediffComp_MechaniteTherapy parentComp);
 
     public virtual void ExposeData()
     {
-        _loadID ??= GetUniqueLoadID();
-        Scribe_Values.Look(ref _loadID, "loadID", null, false);
+        // override as needed
     }
-
-    public string GetUniqueLoadID() => _loadID ??= this.GenerateUniqueLoadID();
 }
