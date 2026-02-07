a78221d8a03a62c6a1597a4b10ff3f271e6e1ea7
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2026-02-07 20:28:57 +0100

    fix powershell install script

--- Source/MoreInjuries/MoreInjuries/_build/install-dependencies.ps1
+++ Source/MoreInjuries/MoreInjuries/_build/install-dependencies.ps1
@@ -12,21 +12,21 @@ $steam_root = $config.steam_root
 $deps_config = Get-Content -LiteralPath "${PSScriptRoot}/dependencies.json" -Raw | ConvertFrom-Json
 
 # Expand workshop_root template
-$workshop_root = $deps_config.workshop_root -replace '\{$steam_root\}', [Regex]::Escape($steam_root) -replace '\\\\', '\'
+$workshop_root = $deps_config.workshop_root -replace '\{steam_root\}', [Regex]::Escape($steam_root) -replace '\\\\', '\'
 
-dest_dir = "${scriptRoot}/dependencies"
-New-Item -ItemType Directory -Path dest_dir -Force | Out-Null
+$dest_dir = "${PSScriptRoot}/dependencies"
+New-Item -ItemType Directory -Path $dest_dir -Force | Out-Null
 
 foreach ($dep in $deps_config.dependencies) {
     $name = $dep.name
 
     $src_path = $dep.path `
-        -replace '\{steam_root\}', [Regex]::Escape($steam_root) `
-        -replace '\{workshop_root\}', [Regex]::Escape($workshop_root) `
-        -replace '\{name\}', [Regex]::Escape($name) `
+        -replace '\{steam_root\}', $steam_root `
+        -replace '\{workshop_root\}', $workshop_root `
+        -replace '\{name\}', $name `
         -replace '\\\\', '\'
 
-    $dest_path = Join-Path dest_dir $name
+    $dest_path = Join-Path $dest_dir $name
 
     Copy-Item -LiteralPath $src_path -Destination $dest_path
 }
