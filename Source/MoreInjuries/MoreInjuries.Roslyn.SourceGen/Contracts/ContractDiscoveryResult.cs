542f896bdf4e9bca30b3c1b2b62a79ccc6bc3c80
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2026-05-03 21:59:06 +0200

    delete temporary changes

--- Source/MoreInjuries/MoreInjuries.Roslyn.SourceGen/Contracts/ContractDiscoveryResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.CodeAnalysis;
-using System.Collections.Immutable;
-
-namespace MoreInjuries.Roslyn.SourceGen.Contracts;
-
-internal sealed record ContractDiscoveryResult<TContract>(
-    Dictionary<TContract, INamedTypeSymbol> Contracts,
-    ImmutableArray<Diagnostic> Diagnostics)
-    where TContract : unmanaged, Enum;
--- Source/MoreInjuries/MoreInjuries.Roslyn.SourceGen/Contracts/GeneratorContractRegistrationAttribute`1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MoreInjuries.Roslyn.SourceGen.Contracts;
-
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false, Inherited = false)]
-internal sealed class GeneratorContractRegistrationAttribute<T>(T type) : Attribute where T : unmanaged, Enum
-{
-    public T Type { get; } = type;
-}
