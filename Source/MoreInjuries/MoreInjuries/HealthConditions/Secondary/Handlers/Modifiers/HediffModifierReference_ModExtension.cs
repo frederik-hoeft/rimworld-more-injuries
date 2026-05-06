a4e1c2acdcd4b7ca5487c9bad9fd0d8cf876c3c0
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2026-05-06 21:17:00 +0200

    continue migration

--- Source/MoreInjuries/MoreInjuries/HealthConditions/Secondary/Handlers/Modifiers/HediffModifierReference_ModExtension.cs
+++ Source/MoreInjuries/MoreInjuries/HealthConditions/Secondary/Handlers/Modifiers/HediffModifierReference_ModExtension.cs
@@ -1,14 +1,11 @@
-﻿using Verse;
+﻿using MoreInjuries.Roslyn.SourceGen.XmlSerialization.Attributes;
+using Verse;
 
 namespace MoreInjuries.HealthConditions.Secondary.Handlers.Modifiers;
 
-// members initialized via XML defs
-[SuppressMessage(CODE_STYLE, STYLE_IDE0032_USE_AUTO_PROPERTY, Justification = JUSTIFY_IDE0032_XML_DEF_REQUIRES_FIELD)]
-[SuppressMessage(CODE_STYLE, STYLE_IDE1006_NAMING_STYLES, Justification = JUSTIFY_IDE1006_XML_NAMING_CONVENTION)]
-public sealed class HediffModifierReference_ModExtension : DefModExtension
+[XmlSerializable]
+public sealed partial class HediffModifierReference_ModExtension : DefModExtension
 {
-    // don't rename this field. XML defs depend on this name
-    private readonly SecondaryHediffModifier modifier = default!;
-
-    public SecondaryHediffModifier Modifier => modifier ?? throw new InvalidOperationException("Modifier is not initialized. Please check your XML definition.");
+    [XmlMember("modifier")]
+    public partial SecondaryHediffModifier Modifier { get; }
 }
